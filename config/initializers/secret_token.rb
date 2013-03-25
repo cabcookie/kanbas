df89b7a4bddfbd070647ab7a571ddfa149329cf3
Carsten Koch <carsten.b.koch@gmail.com>
2013-03-25 15:08:45 +0100

    Initial commit with static files for testing the drawing functionality on dekstop and tablet.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kanbas::Application.config.secret_token = '3fd40f8db26e81fedbe0b0038bff5257afdc61decd11488a7985d69f06f52407f7666f658fa6ac507a74c1934888da716ba70cf034ca807a657ef2d85c3fa27d'
